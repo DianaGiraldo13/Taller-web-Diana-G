32207d3c7ade101f6894997e1742e8584d128567
Diana Giraldo <dianasofiagiraldobotello@gmail.com>
2021-03-18 11:40:20 -0500

    Se implemento la interaccion de los colores de las cajitas y se hicieron algunos cambios

--- script/script.js
+++ script/script.js
@@ -25,3 +25,67 @@ function showSlides(n) {
 function moveToSubscribe(){
     document.getElementById("subscribe").focus();
 }
+
+
+//interacción
+
+
+const imagenes__cajita = document.getElementsByClassName('imagenes__cajita');
+
+const imagen__circuloamarillo = document.querySelector('.imagen__circuloamarillo');
+imagen__circuloamarillo.addEventListener('click',clickAmarillo);
+
+
+function clickAmarillo (){
+    imagenes__cajita[0].setAttribute('src','./../cajaamarilla.png')
+}
+
+//otracaja
+
+const imagen__circulonegro = document.querySelector('.imagen__circulonegro');
+imagen__circulonegro.addEventListener('click',clickNegro);
+
+
+function clickNegro (){
+    imagenes__cajita[0].setAttribute('src','./../cajanegra.png')
+
+}
+
+//otra caja
+
+const imagen__circuloverde = document.querySelector('.imagen__circuloverde');
+imagen__circuloverde.addEventListener('click',clickVerde);
+
+
+function clickVerde (){
+    imagenes__cajita[0].setAttribute('src','./../cajaverde.png')
+
+}
+
+//otra caja
+
+const imagen__circulonaranja = document.querySelector('.imagen__circulonaranja');
+imagen__circulonaranja.addEventListener('click',clickNaranja);
+
+
+function clickNaranja (){
+    imagenes__cajita[0].setAttribute('src','./../cajanaranja.png')
+
+}
+
+//otra caja
+
+const imagen__circuloazul = document.querySelector('.imagen__circuloazul');
+imagen__circuloazul.addEventListener('click',clickAzul);
+
+
+function clickAzul (){
+    imagenes__cajita[0].setAttribute('src','./../cajaazul.png')
+
+}
+
+
+
+
+
+
