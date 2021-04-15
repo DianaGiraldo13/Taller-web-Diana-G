eaa933dda5008273e46ed9f2c9a8c20de5b216bc
Diana Giraldo <dianasofiagiraldobotello@gmail.com>
2021-04-15 12:06:43 -0500

    Se implemento el arreglo de los productos

---
+++ script/productos.js
@@ -0,0 +1,82 @@
+const productos = [
+    {
+        img: 'Doll_Face-removebg-preview.png',
+        titulo: 'loremd',
+        precio: 44000,
+    },
+
+    {
+        img: 'Doll_Face-removebg-preview.png',
+        titulo: 'loremeeeee',
+        precio: 49000,
+    },
+
+    {
+        img: 'Doll_Face-removebg-preview.png',
+        titulo: 'loremmmmmm',
+        precio: 40000,
+    },
+
+    {
+        img: 'Doll_Face-removebg-preview.png',
+        titulo: 'loremeeeee',
+        precio: 49000,
+    },
+
+    {
+        img: 'Doll_Face-removebg-preview.png',
+        titulo: 'loremmmmmm',
+        precio: 40000,
+    },
+
+    {
+        img: 'Doll_Face-removebg-preview.png',
+        titulo: 'loremeeeee',
+        precio: 49000,
+    },
+
+    {
+        img: 'Doll_Face-removebg-preview.png',
+        titulo: 'loremmmmmm',
+        precio: 40000,
+    },
+
+
+]
+
+const list = document.querySelector('.list');
+
+function handleProductoitem(item){
+    
+        const productos = document.createElement('a');
+        productos.innerHTML = `
+        
+        <img class="producto__imagen" src="${item.img}" alt="">
+
+        <div class="producto__info">
+
+            <h1 class="producto__titulo">
+
+                ${item.titulo}
+
+            </h1>
+
+            <h3 class="producto__precio">
+                
+                ${item.precio}
+
+            </h3>
+
+
+        </div>
+
+        `;
+
+        productos.classList.add('productos');
+        productos.setAttribute('href', '#');
+        
+        list.appendChild(productos);
+
+}
+
+productos.forEach(handleProductoitem)
