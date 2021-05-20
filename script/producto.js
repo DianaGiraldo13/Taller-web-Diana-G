894c67e1319887faada603a392713de8d4405757
Sok4h <dianasofiagiraldobotello@gmail.com>
2021-05-20 16:37:54 -0500

    cambio imagen producto

--- script/producto.js
+++ script/producto.js
@@ -7,6 +7,58 @@ const queEs = document.querySelector(".productoQueEs")
 const modoDeUso = document.querySelector(".productoModoDeUso")
 const descripcion = document.querySelector(".productoDescripcion")
 const encanta = document.querySelector(".productoEncanta")
+const tituloEncanta = document.querySelector(".tituloEncanta");
+const flechaDerechaProducto= document.querySelector(".flechaDerechaProducto")
+const flechaIzquierdaProducto= document.querySelector(".flechaIzquierdaProducto")
+let  productoActual;
+let imagenActual=0;
+
+
+
+
+
+
+flechaIzquierdaProducto.addEventListener("click",()=>{
+    
+     
+    if(imagenActual-- > 0){
+
+        imagenActual-1
+        
+        imagen.src=productoActual.imagenes[imagenActual].url
+         
+    } else{
+
+        console.log("no funciona")
+        imagenActual=0;
+    }
+    
+    
+});
+
+flechaDerechaProducto.addEventListener("click",()=>{
+   
+
+    if(imagenActual+1 < productoActual.imagenes.length){
+
+        imagenActual= imagenActual+1
+        imagen.src=productoActual.imagenes[imagenActual].url
+         
+    }
+    else{
+
+        imagenActual=productoActual.imagenes.length-1;
+    }
+    
+
+});
+
+function updateImage(){
+
+    imagen.src=productoActual.imagenes[imagenActual].url
+
+}
+
 
 const id = params.get("id")
 
@@ -18,25 +70,34 @@ if(!id){
 db.collection("products").doc(id).get().then(function(doc){
 
     const data = doc.data();
-    
-    imagen.src=data.imagenes[0].url
+    productoActual=data;
     marca.innerText=data.marca
+    imagen.src=data.imagenes[imagenActual].url
     nombre.innerText = data.nombre
     precio.innerText = "$"+data.precio
     let divido = data.descripcion.split("/")
-    console.log(divido)
     queEs.innerText=divido[0]
     modoDeUso.innerText=divido[1]
-    let splitEncanta = divido[2].split("•")
-    console.log(splitEncanta)
-    //encanta.innerText=
+  
 
-    for(let i=1;i<splitEncanta.length;i++){
+    // verifica si tiene seccion lo que nos encanta
+    if(divido[2]){
 
-        let listaEncanta = document.createElement("p")
-        listaEncanta.innerText="•"+splitEncanta[i]
-        encanta.appendChild(listaEncanta)
+        let splitEncanta = divido[2].split("•")
+        for(let i=1;i<splitEncanta.length;i++){
+
+            let listaEncanta = document.createElement("p")
+            listaEncanta.innerText="•"+splitEncanta[i]
+            encanta.appendChild(listaEncanta)
+        }
     }
+    else{
+
+        tituloEncanta.style.display="none";
+
+    }
+    
+    
 
     
 })
